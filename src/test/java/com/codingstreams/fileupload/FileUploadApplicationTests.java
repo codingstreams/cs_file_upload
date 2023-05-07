b23b1a43b511c0f031f69144920108d95c5470fb
Coding Streams <akxhdev@outlook.com>
2023-05-07 11:14:00 +0530

    Initial commit

---
+++ src/test/java/com/codingstreams/fileupload/FileUploadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingstreams.fileupload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileUploadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
